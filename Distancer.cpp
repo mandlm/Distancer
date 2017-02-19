c7250b33affd9c4cc6556989904e236e48a6cd3e
Michael Mandl <mandlm@molez.net>
2017-02-19 15:04:43 +0100

    Added a flashing status led to pin PB0

--- Distancer.cpp
+++ Distancer.cpp
@@ -1,10 +1,29 @@
+#define F_CPU 1000000
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 #include "DeviceLib/ShiftRegister.h"
 
+void setupStatusLedPin()
+{
+	DDRB |= (1 << PB0);
+}
+
+void flashStatusLed()
+{
+	PORTB |= (1 << PB0);
+	_delay_ms(100);
+	PORTB &= ~(1 << PB0);
+}
+
 int main()
 {
+	setupStatusLedPin();
+
 	while (true)
 	{
+		flashStatusLed();
+		_delay_ms(400);
 	}
 }
