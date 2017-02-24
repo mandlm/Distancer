f7393f0661eb12d2feeabc60fc7a9ba37b621774
Michael Mandl <mandlm@molez.net>
2017-02-24 21:48:01 +0100

    Count up seconds on display

--- Distancer.cpp
+++ Distancer.cpp
@@ -1,46 +1,27 @@
-#define F_CPU 1000000
+#include "Hardware.h"
+#include "MyLcd.h"
 
 #include <avr/io.h>
-#include <util/delay.h>
 
-#include "LcdShiftReg.h"
-
-void setupStatusLed()
+extern "C" void __cxa_pure_virtual(void)
 {
-	DDRB |= (1 << PB0);
-}
-
-void flashStatusLed()
-{
-	PORTB |= (1 << PB0);
-	_delay_ms(100);
-	PORTB &= ~(1 << PB0);
+	
 }
 
 int main()
 {
-	setupStatusLed();
-
-	LcdShiftReg lcdShiftReg;
+	MyLcd lcd;
+	delay_ms(40);
+	lcd.initDisplay();
+	
+	uint16_t counter = 0;
 
 	while (true)
 	{
-		flashStatusLed();
-		_delay_ms(400);
+		lcd.clear();
+		lcd.output(counter++);
+		lcd.output('s');
 
-		lcdShiftReg.set(1);
-		_delay_ms(100);
-		lcdShiftReg.set(0);
-		_delay_ms(400);
-
-		lcdShiftReg.set(1 << 1);
-		_delay_ms(100);
-		lcdShiftReg.set(0);
-		_delay_ms(400);
-
-		lcdShiftReg.set(1 << 7);
-		_delay_ms(100);
-		lcdShiftReg.set(0);
-		_delay_ms(400);
+		delay_ms(1000);
 	}
 }
