903d8f020a73c2f4b903ff8626aa9935f8308533
Michael Mandl <mandlm@molez.net>
2017-03-02 08:19:00 +0100

    Output elapsed time to lcd

--- Distancer.cpp
+++ Distancer.cpp
@@ -1,8 +1,6 @@
 #include "Hardware.h"
 #include "MyLcd.h"
 
-#include <avr/io.h>
-
 extern "C" void __cxa_pure_virtual(void)
 {
 	
@@ -11,17 +9,25 @@ extern "C" void __cxa_pure_virtual(void)
 int main()
 {
 	MyLcd lcd;
-	delay_ms(40);
+	delay_us(40e3);
 	lcd.initDisplay();
 	
 	uint16_t counter = 0;
 
 	while (true)
 	{
-		lcd.clear();
-		lcd.output(counter++);
-		lcd.output('s');
+		lcd.clear();		
+		
+		lcd.output(counter / 60);
+		lcd.output("m " );
+		lcd.output(counter % 60);
+		lcd.output("s");
 
-		delay_ms(1000);
+		++counter;
+		
+		for (uint8_t i = 0; i < 20; ++i)
+		{
+			delay_us(50e3);
+		}
 	}
 }
